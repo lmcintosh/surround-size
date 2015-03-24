6861782cdc0fcc5aa6242bae8bcfe10fb4c2b88a
Lane McIntosh <lanemcintosh@gmail.com>
2015-03-24 15:12:06 -0700

    Added new python file of miscellaneous image processing functions, including loading images and computing rotationally averaged 2d FFT.

---
+++ image_processing_functions.py
@@ -0,0 +1,88 @@
+from os import listdir
+from os.path import isfile, join
+import array as ar
+import scipy.io
+
+def load_images(path, numImages, patchSize=None, acceptedExtensions=['.imc','LUM.mat'], square=False, normalize='subtract'):
+    ''' Load images and return a list of numpy arrays.'''
+    
+    allfiles = []
+    for ext in acceptedExtensions:
+        allfiles = allfiles + [f for f in listdir(path) if isfile(join(path,f)) and ext in f]
+    if len(allfiles) < numImages:
+        print 'Only ' + str(len(allfiles)) + ' files in directory.'
+    names    = allfiles[:numImages]
+    patches  = []
+
+    for im in names:
+        if '.imc' in im:
+            fin = open(path+'/'+im, 'rb')
+            s   = fin.read()
+            fin.close()
+            arr = ar.array('H', s)
+            arr.byteswap()
+            img = np.array(arr, dtype='uint16').reshape(1024,1536)
+        elif '.mat' in im:
+            fin = scipy.io.loadmat(path+'/'+im)
+            arr = fin['LUM_Image']
+            # make 0 to 255
+            arr = arr - min(arr.flat)
+            arr = 255.*arr/max(arr.flat)
+            img = np.array(arr, dtype='uint16')
+            
+        # make patches
+        if patchSize:
+            img_center = [np.shape(img)[0]/2,np.shape(img)[1]/2]
+            img        = img[img_center[0]-patchSize/2:img_center[0]+patchSize/2,img_center[1]-patchSize/2:img_center[1]+patchSize/2]
+            
+        if square:
+            N = min(shape(img))
+            N = N - (N % 2) # make even
+            img = img[:N,:N]
+        
+        if normalize == 'subtract':
+            # normalize to unit variance and zero mean (originally I did these in opposite order, but this
+            # is the official z-scoring order I believe)
+            img -= np.mean(img)
+            patches.append(img/np.std(img))
+        elif normalize == 'divisive':
+            # normalize by the mean
+            patches.append(img/np.mean(img))
+        else:
+            patches.append(img)
+
+    return patches
+
+def cart2pol(x, y):
+    rho = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return (phi, rho)
+
+def rotavg(im):
+    '''im can be square 2d numpy array'''
+    imShape = shape(im) # NNM
+    N       = imShape[0]
+    X, Y    = np.meshgrid(range(-N//2,N//2),range(-N//2,N//2))
+
+    theta, rho = cart2pol(X,Y);
+    rho = rho.round()
+    f   = [mean(im[np.where(rho==r)]) for r in range(N//2+1)] 
+        
+    return f
+
+def spectrum2d(arr, Fs=1000.0, frequencyFlag=True):
+    amp = abs(fftshift(fft2(arr)))/np.prod(arr.shape)
+    amp = rotavg(amp)
+    n = len(amp)
+    #amp = amp/n
+    k = arange(n)
+    T = n/Fs
+    frq = k/(2*T) # two sides frequency range
+    #frq = frq[range(n/2)] # one side frequency range
+    
+    if frequencyFlag:
+        #return (frq, [a/n**2 for a in amp])  # should n be squared?
+        return (frq, amp)
+    else:
+        #return [a/n**2 for a in amp]  # should n be squared?
+        return amp
