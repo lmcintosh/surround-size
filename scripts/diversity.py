84b3497a0c6b92fc95f7e9d331f280ca5c557019
Lane McIntosh <lmcintosh@stanford.edu>
2017-10-27 19:15:39 -0700

    changes to diversity.py to reduce memory usage

--- scripts/diversity.py
+++ scripts/diversity.py
@@ -32,13 +32,13 @@ from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 
 # LOAD CONSTANTS AND DATA
-signal = np.array(np.load('signal_3_23.npy'))
-constants = np.load('2017_10_9_diversity_constants.npy').item()
-fits = np.load('fits_3_23.npy').item()
-all_params = np.load('params_3_23.npy').item()
-variances = np.load('variances_3_23.npy').item()
-mean_squared_errors = np.load('mse_3_23.npy').item()
-abs_errors = np.load('abserrs_3_23.npy').item()
+signal = np.array(np.load(os.path.expanduser('~/data/surround/signal_3_23.npy')))
+constants = np.load(os.path.expanduser('~/data/surround/2017_10_9_diversity_constants.npy')).item()
+fits = np.load(os.path.expanduser('~/data/surround/fits_3_23.npy')).item()
+all_params = np.load(os.path.expanduser('~/data/surround/params_3_23.npy')).item()
+variances = np.load(os.path.expanduser('~/data/surround/variances_3_23.npy')).item()
+mean_squared_errors = np.load(os.path.expanduser('~/data/surround/mse_3_23.npy')).item()
+abs_errors = np.load(os.path.expanduser('~/data/surround/abserrs_3_23.npy')).item()
 
 
 def generate_spatial_signals(batch_size, signal=signal):
@@ -64,28 +64,21 @@ def center_and_surround(space, center_width, surround_width, center_strength, su
     return diff_of_gauss_mu0(space, abs(center_width), abs(surround_width),
                             -abs(center_strength), abs(surround_strength))
 
-batch_size = 128
+batch_size = 320 
 max_channel_depth = 10
 decoder_size = 200
 x = generate_spatial_signals(batch_size)
 input_len = x.shape[1]
 output_len = x.shape[1]
-max_steps = 100000
-lr = 0.01
-gain = 0.6
-target_snrs = [0.01, 1.0, 100.0]
-both_results = {{
-    'random': collections.defaultdict(list),
-    'homogenous': collections.defaultdict(list)
-} for t in target_snrs}
+max_steps = 10000
+lr = 0.002
+gain = 1.0   # 0.5
+target_snrs = [100.0]
 
 with tf.device('/gpu:0'):
     for target_snr in target_snrs:
-        for random_initialization in [True, False]:
-            mean_errors = []
-            sem_errors = []
+        for random_initialization in [False, True]:
             all_results = []
-            all_errors = []
             for channel_depth in range(1, max_channel_depth+1):
                 # Universal variables
                 results = collections.defaultdict(list)
@@ -110,11 +103,14 @@ with tf.device('/gpu:0'):
                     # Horizontal weight parameters.
                     if random_initialization:
                         hw_param = tf.get_variable('horz_weights', shape=(channel_depth,), dtype=tf.float32,
-                                initializer=tf.random_uniform_initializer(minval=-5, maxval=5))
+                                initializer=tf.random_uniform_initializer(minval=-1, maxval=1))
                         ideal_horz_weights = tf.nn.sigmoid(gain * hw_param)  # hw_param
                     else:
-                        hw_param = tf.constant(0.144, dtype=tf.float32, shape=(channel_depth,), name='horz_weights')
-                        ideal_horz_weights = hw_param # tf.nn.sigmoid(gain * hw_param)
+                        hw_param = tf.get_variable('horz_weights', shape=(1,), dtype=tf.float32,
+                                initializer=tf.random_uniform_initializer(minval=-1, maxval=1))
+                        ideal_horz_weights = tf.nn.sigmoid(gain * hw_param)  # hw_param
+                        # hw_param = tf.constant(0.144, dtype=tf.float32, shape=(channel_depth,), name='horz_weights')
+                        # ideal_horz_weights = hw_param # tf.nn.sigmoid(gain * hw_param)
 
                     # Center weight parameters.
                     cw_param = tf.get_variable('center_weights', shape=(channel_depth,), dtype=tf.float32,
@@ -125,7 +121,10 @@ with tf.device('/gpu:0'):
                     filtered_output = []
                     for c in range(channel_depth):
                         # e = encoder(ideal_horz_weights[c], ideal_center_weights[c])
-                        filters.append(encoder(ideal_horz_weights[c], ideal_center_weights[c]))
+                        if random_initialization:
+                            filters.append(encoder(ideal_horz_weights[c], ideal_center_weights[c]))
+                        else:
+                            filters.append(encoder(ideal_horz_weights[0], ideal_center_weights[c]))
                     kernel = tf.stack(filters, axis=-1)
                     kernel = tf.expand_dims(kernel, axis=1)
                     print('Kernel has shape %s.' %(kernel.shape,))
@@ -167,20 +166,14 @@ with tf.device('/gpu:0'):
                                 [train_op, mse, kernel, ideal_horz_weights, ideal_center_weights,
                                  weights, snr, snr_regularization, n_in, n_out], feed_dict={label: y})
                             errors.append(error)
-                            if step % 1000 == 0:
+                            if step % 100 == 0:
                                 print('Error at step %04d is %0.4f' %(step, error))
-                                results['input'].append(x)
-                                results['labels'].append(y)
+                            if step % 5000 == 0:
                                 output = sess.run([out], feed_dict={label: y})[0]
-                                results['output'].append(output)
-                                results['kernel'].append(k)
                                 results['hw'].append(hw)
                                 results['cw'].append(cw)
-                                results['decoder'].append(decoder)
                                 results['snr'].append(this_snr)
                                 results['snr_reg'].append(snr_reg)
-                                results['input_noise'].append(ni)
-                                results['output_noise'].append(no)
                             elif step == max_steps - 1:
                                 print('Error at step %04d is %0.4f' %(step, error))
                                 results['input'].append(x)
@@ -204,20 +197,19 @@ with tf.device('/gpu:0'):
                 # this_err = sem([np.mean(
                 #    (results['output'][-1][j] - results['labels'][-1][j])**2) for j in range(batch_size)])
 
-                if random_initialization:
-                    random_flag = 'random'
-                else:
-                    random_flag = 'homogenous'
-
-                both_results[snr][random_flag]['mean_errors'].append(this_mse)
-                both_results[snr][random_flag]['sem_errors'].append(this_err)
-                both_results[snr][random_flag]['results'].append(results)
-                both_results[snr][random_flag]['errors'].append(errors)
-                both_results[snr][random_flag]['channels'].append(channel_depth)
+                results['errors'] = errors
+                results['mean_errors'] = this_mse
+                results['sem_errors'] = this_err
+                results['channels'] = channel_depth
+                all_results.append(results)
 
                 tf.reset_default_graph()
 
+            if random_initialization:
+                np.save('/home/lane/code/ipython-notebooks/baccuslab/2017_10_27_diversity_random_results.npy', all_results)
+            else:
+                np.save('/home/lane/code/ipython-notebooks/baccuslab/2017_10_27_diversity_homogenous_results.npy', all_results)
 
-np.save('/home/lane/code/ipython-notebooks/baccuslab/2017_10_14_diversity_both_results.npy', both_results)
+
 
 
